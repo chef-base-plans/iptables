88fbf9c1cbdd113ffaa6977104a1f548ed5d692c
Lucas Giancola <lucas.giancola@live.com>
2021-03-17 12:02:28 -0400

    Bumped version 1.8.4 -> 1.8.7

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=iptables
 pkg_origin=core
-pkg_version=1.8.4
+pkg_version=1.8.7
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0-or-later')
 pkg_source="http://netfilter.org/projects/iptables/files/${pkg_name}-${pkg_version}.tar.bz2"
 pkg_upstream_url="http://netfilter.org/projects/iptables"
 pkg_description="iptables is the userspace command line program used to configure the \
   Linux 2.4.x and later packet filtering ruleset"
-pkg_shasum=993a3a5490a544c2cbf2ef15cf7e7ed21af1845baf228318d5c36ef8827e157c
+pkg_shasum=c109c96bb04998cd44156622d36f8e04b140701ec60531a10668cfdff5e8d8f0
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/make
